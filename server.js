ec1f7455dd162f0ad6de6ad63a20b20a0e22ff4c
Mohammed <mohammedalsubaie14@gmail.com>
2022-02-18 03:52:13 -0800

    add check endpointadd check endpointadd check endpoint

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json())
+const obj = [
+   {id: 1 , nameOne : 'Mohammad'},
+   {id: 2 , nameTwo : 'Bassam'}
+]
 
-app.get('/Hello',(req,res)=>{
-    res.send('Hello bassam & Mohammad ')
+app.get('/',(req,res)=>{
+    res.send('go to hello path ')
+})
+
+app.get('/hello',(req,res)=>{
+    console.log(res.send('Hello bassam & Mohammad '))
+    res.send(obj)
 });
 
+// app.get('/hello/check/:id', (req,res)=>{
+//     //res.send(req.params.name)
+//     const objFind = obj.find(c => c.id === parseInt(req.params.name))
+//     if(!objFind) res.status(404).send('Ooh somthing wrong')
+//     res.send(objFind)
+// })
 
-app.listen(1221,()=>{
-    console.log('PORT:1221')
+app.post('/hello/check',(req,res)=>{
+    console.log(req.body.per)
+    res.send('Im a postman')
 })
+
+const port= process.env.PORT || 1221
+app.listen(port, ()=> console.log(`listen to ${port}`));
